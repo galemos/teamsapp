705929de06708f8403dbb48fde4858db40f8af16
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2023-03-22 22:40:22 -0300

    criando-componente-group-card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,18 @@
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight
+        title='Turmas'
+        subTitle='Jogue com a sua turma'
+      />
+      <GroupCard title='Gale4ra do Ignite' />
     </Container>
   );
 }
