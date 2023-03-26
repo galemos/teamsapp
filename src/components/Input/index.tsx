096901b66424905d3299039641f0209e5017408f
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2023-03-26 13:21:25 -0300

    input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import { TextInputProps } from 'react-native'
+import { useTheme } from 'styled-components/native'
+
+import { Container } from './styles'
+
+type Props = TextInputProps & {
+}
+
+export function Input({ ...rest }: Props) {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container
+      placeholderTextColor={COLORS.GRAY_300}
+      {...rest}
+    />
+  )
+}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from './styles'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 
 
 type Props = {
@@ -14,12 +15,19 @@ export function NewGroup({ }: Props) {
       <Header showBackButton />
       <Content>
         <Icon />
+
         <Highlight
           title='Nova Turma'
           subTitle='cria a turma para adicionar as pessoas'
         />
+
+        <Input
+          placeholder='Nome da turma'
+        />
+
         <Button
           title='Criar'
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
