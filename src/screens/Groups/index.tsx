ada5d2a9d15eaefb848b0cedd126437e99b12028
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2023-03-26 12:05:52 -0300

    button and listempty componnents

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
@@ -12,7 +18,24 @@ export function Groups() {
         title='Turmas'
         subTitle='Jogue com a sua turma'
       />
-      <GroupCard title='Gale4ra do Ignite' />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Cadastrar a primeira turma' />
+        )}
+      />
+
+      <Button
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
