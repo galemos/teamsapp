925e0fe8d7628a0d8b9e0f98ccd5ae3632752dfd
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2023-03-30 23:20:49 -0300

    commit

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 import { Loading } from '@components/Loading';
 
 import theme from './src/theme'
@@ -21,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoades ? <NewGroup /> : <Loading />}
+      {fontsLoades ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
